e8053edef529a3aa694c526142e9127012bec280
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-11-28 22:40:25 +0530

    add user model with schema, password hashing, and validation methods

---
+++ models/user.js
@@ -0,0 +1,58 @@
+// # User Model #
+
+// # Import Required Modules
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const createHttpError = require('http-errors');
+const { roles } = require('../utils/constants');
+
+// # Define User Schema
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: [roles.admin, roles.moderator, roles.client],
+    default: roles.client
+  }
+});
+
+// # Hash Password Before Saving User
+UserSchema.pre('save', async function (next) {
+  try {
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+      if (this.email === process.env.ADMIN_EMAIL.toLowerCase()) {
+        this.role = roles.admin;
+      }
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// # Check Password Validity For User Login 
+UserSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw createHttpError.InternalServerError(error.message);
+  }
+};
+
+// # Create User Model
+const User = mongoose.model('user', UserSchema);
+
+// # Export User Model
+module.exports = User;
